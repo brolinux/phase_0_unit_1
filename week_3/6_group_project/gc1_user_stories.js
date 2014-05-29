9e2113d7ff0a22f7aadc218926c78ec52587b79c
dbc ariel <dbcariel14@gmail.com>
2014-05-29 15:27:11 -0400

    Update and rename my_solution.js to gc1_user_stories.js

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,103 @@
+// My role in the group is: to tell a story and pass it to 2nd member
+
+// Here is my part of the challenge:
+
+The Story:
+
+I want to create two sets of numbers and call these sets by two unique names;
+I want one set to be known as "oddLengthArray" and have the number elements 1,2,3,4,5,5,7 be assigned to it;
+I want another set to be known as "evenLengthArray" and have the number elements 4,4,5,5,6,6,6,7 be assigned to it;
+
+********************************
+
+Create a function called "assert" which equals three unique pieces of information inside of it;
+The function "assert" needs to equal three place holders with specific labels:
+1. The mathematical outcome to a series of calculations that we will run in a test, we can label this place holder "test";
+2. A worded sentence, we can label this place holder "message";
+3. The numerical order of the test that's being run, we can label this place holder "test_number";
+
+If the answer in the test is an incorrect answer, I want an three pieces of information to appear on the screen:
+1. The number of the test taken or "test_number";
+2. The word "false" next to the "test_number";
+3. The word "Error:" with a worded sentence which we earlier said would be labeled "message";
+
+
+If the answer given is correct two pieces of information need to appear on the screen:
+1. The number of the test taken, or "test_number";
+2. The word "true" next to it;
+
+********************************
+********************************
+The 1st test to run through assert is a test called "sum", all three values need to be placed inside their repsective place holders inside assert:
+1. sum needs to calculate a mathematic problem of adding all the numbers inside a set, where the final answer appears in the function assert under the value "test";
+2. sum's worded sentence, sage, is "sum should be a Function." this message needs to run through assert under the term "message";
+3. This is test number 1. The value 1 needs to appear inside assert under the label "test_number".
+
+The 2nd test assert runs will take the calculations of sum and run it for the values inside the set "oddLengthArray" returning three pieces of information:
+1. sum's test result for set "oddLengthArray" should be number 27;
+2. The message for this test is "sum should return the sum of all elements in an array with an odd length." this message needs to run through assert under the place
+
+holder "message";
+3. This is test number 2. The value 2 needs to appear inside assert under the label "test_number".
+
+
+The 3rd test assert runs will take the calculations of sum and run it for the values inside the set "evenLengthArray" returning three pieces of information:
+1. sum's test result for set "evenLengthArray" should be number 43;
+2. The message for this test is "sum should return the sum of all elements in an array with an even length." this message needs to run through assert under the
+
+place holder "message";
+3. This is test number 3. The value 3 needs to appear inside assert under the label "test_number".
+
+********************************
+********************************
+
+
+The 4th test to run through assert is a test called "mean", all three values need to be placed inside their repsective place holders inside assert:
+1. mean calculates the mathematic mean of a set of numbers,the result of which appears in the function assert under the value "test";.
+2. means's message is "mean should be a Function." this message needs to run through assert under the term "message";
+3. This is test number 4. The value 4 needs to appear inside assert under the label "test_number".
+
+
+The 5th test assert runs will take the calculations of mean and run it for the values inside the set "oddLengthArray" returning three pieces of information:
+1. mean's test result for set "oddLengthArray" should be number 3.857142857142857;
+2. The message for this test is "mean should return the average of all elements in an array with an odd length." this message needs to run through assert under the
+
+place holder "message";
+3. This is test number 5. The value 5 needs to appear inside assert under the label "test_number".
+
+
+The 6th test assert runs will take the calculations of mean and run it for the values inside the set "evenLengthArray" returning three pieces of information:
+1. mean's test result for set "evenLengthArray" should be number 5.375;
+2. The message for this test is "mean should return the average of all elements in an array with an odd length." this message needs to run through assert under the
+
+place holder "message";
+3. This is test number 6. The value 6 needs to appear inside assert under the label "test_number".
+
+
+********************************
+********************************
+
+The 7th test to run through assert is a test called "median", all three values need to be placed inside their repsective place holders inside assert:
+1. median calculates the mathematic median of a set of numbers,the result of which appears in the function assert under the value "test";.
+2. median's message is "median should be a Function." this message needs to run through assert under the term "message";
+3. This is test number 7. The value 7 needs to appear inside assert under the label "test_number".
+
+
+The 8th test assert runs will take the calculations of median and run it for the values inside the set "oddLengthArray" returning three pieces of information:
+1. median's test result for set "oddLengthArray" should be number 4;
+2. The message for this test is "median should return the median value of all elements in an array with an odd length." this message needs to run through assert
+
+under the place holder "message";
+3. This is test number 8. The value 8 needs to appear inside assert under the label "test_number".
+
+
+The 9th test assert runs will take the calculations of mean and run it for the values inside the set "evenLengthArray" returning three pieces of information:
+1. median's test result for set "evenLengthArray" should be number 5.5;
+2. The message for this test is "median should return the median value of all elements in an array with an even length.", this message needs to run through assert
+
+under the place holder "message";
+3. This is test number 9. The value 9 needs to appear inside assert under the label "test_number".
+
+
+
+********************************
